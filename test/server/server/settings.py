1ab11efcf995826ea0b1d53f28649f9d9d33cb44
VishalTheHuman <vishalatmadurai@outlook.com>
2023-12-19 12:55:18 +0530

    Super Pre-Final

--- test/server/server/settings.py
+++ test/server/server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^z^sr&-(0a)oo4ra-^ioae^vom5ju$pl!=!@6$$8bt5r^(ic9%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
